b21a2e3b096c12e76d770b4fe6bbb692d25e4c65
YDProject0 <your.dream848@gmail.com>
2022-09-14 22:40:36 +0700

    (+) fade in effect

--- js/index.js
+++ js/index.js
@@ -150,3 +150,18 @@ function controlAudioSound() {
         $('#au').prop("currentTime", 0);
     }
 }
+
+$(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".content");
+  
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
+        if ($(tag).position().top < pageBottom) {
+          $(tag).addClass("visible");
+        } else {
+          $(tag).removeClass("visible");
+        }
+    }
+});
